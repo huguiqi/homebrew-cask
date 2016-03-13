5bf7c244f7b832e7b4cd04e21e150bf80d9e979f
sam.hu <sam@samhus-MBP.lan>
2016-03-14 00:33:54 +0800

    add readkit

---
+++ Casks/readkit.rb
@@ -0,0 +1,11 @@
+cask 'readkit' do
+  version 'latest'
+  sha256 'c62a0ab7b1c04cb1e999911371d3b6093505112c'
+
+  url 'http://readkitapp.com/download/ReadKit-Trial.zip'
+  name 'readkit'
+  homepage 'http://readkitapp.com/'
+  license :closed # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
+
+  app '/Users/sam/Downloads/好照片\ mac中文版v1.4.1.dmg'
+end
